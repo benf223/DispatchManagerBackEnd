0ffe7fa43479c7fd4478b6d9ba32bd09a642b3c8
benf223 <ppj1483@autuni.ac.nz>
2018-05-10 21:14:04 +1200

    Added Sample tests

---
+++ server.js
@@ -0,0 +1,6 @@
+function sample(a, b)
+{
+	return a + b;
+}
+
+module.exports = sample;
---
+++ test/test.js
@@ -0,0 +1,15 @@
+var sample = require('../server');
+var expect = require('chai').expect;
+
+describe('sample()', function () {
+	it('should add two numbers', function ()
+	{
+		var x = 3;
+		var y = 3;
+		var sum = x + y;
+
+		var sum2 = sample(x, y);
+
+		expect(sum2).to.be.equal(sum);
+	});
+})
