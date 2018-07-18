40c22c10c1b470af54835c5edd41c48ddc1bc2d0
Neil Read <neil.read14@outlook.com>
2018-07-18 19:03:17 +1200

    Added some error trapping and further unit tests

--- test/journeyEstimator_test.js
+++ test/journeyEstimator_test.js
@@ -3,17 +3,28 @@
  */
 const je = require('../journeyEstimator');
 const expect = require('chai').expect;
+var tomorrow;
+var yesterday;
+
+before(() =>
+{
+    let date = new Date();
+    tomorrow = je.createDate(date.getDate() + 1, date.getMonth() + 1, date.getFullYear(), date.getHours(), date.getMinutes());
+    yesterday = je.createDate(date.getDate() - 1, date.getMonth() + 1, date.getFullYear(), date.getHours(), date.getMinutes());
+});
 
 describe("getTravelTime()", () =>
 {
-    it("should return an estimated travel time (in minutes) between two locations", (done) =>
+    it("should return an estimated travel time (in minutes) between two locations", () =>
     {
-        let date = new Date();
-        console.log(date);
-        date = je.createDate(date.getDate() + 1, date.getMonth(), date.getYear(), date.getHours(), date.getMinutes());
-        je.getTravelTime("55 Wellesley St, Auckland", "90 Akoranga Dr, Northcote", date).then((time) =>
+        return je.getTravelTime("55 Wellesley St, Auckland", "90 Akoranga Dr, Northcote", tomorrow).then((time) =>
         {
-            expect(time).to.be.a("number").and.be.greaterThan(30);
+            expect(time).to.be.a("number").and.be.greaterThan(je.travelOverheadTime);
         });
+    });
+
+    it("should throw an error if an address is invalid", () =>
+    {
+        expect(() => je.getTravelTime("Invalid", "90 Akoranga Dr, Northcote", tomorrow)).to.be.rejectedWith(Error);
     });
 });
