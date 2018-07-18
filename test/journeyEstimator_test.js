6d33d1348f5e4ce4d664f825d3c1bd41f0be9cd0
neilread <31087483+neilread@users.noreply.github.com>
2018-07-19 00:48:15 +1200

    Completed unit tests and error trapping for getTravelTime()

--- test/journeyEstimator_test.js
+++ test/journeyEstimator_test.js
@@ -1,8 +1,14 @@
 /**
  * Author: Neil Read
  */
+var chai = require("chai");
+var chaiAsPromised = require("chai-as-promised");
+
+chai.use(chaiAsPromised);
+
+// Then either:
 const je = require('../journeyEstimator');
-const expect = require('chai').expect;
+const expect = chai.expect;
 var tomorrow;
 var yesterday;
 
@@ -17,14 +23,35 @@ describe("getTravelTime()", () =>
 {
     it("should return an estimated travel time (in minutes) between two locations", () =>
     {
-        return je.getTravelTime("55 Wellesley St, Auckland", "90 Akoranga Dr, Northcote", tomorrow).then((time) =>
+        return expect(je.getTravelTime("55 Wellesley St, Auckland", "90 Akoranga Dr, Northcote", tomorrow)).to.eventually.be.a("number").and.be.greaterThan(je.travelOverheadTime).then(() =>
         {
-            expect(time).to.be.a("number").and.be.greaterThan(je.travelOverheadTime);
+            return expect(je.getTravelTime("Auckland", "Tauranga", tomorrow)).to.eventually.be.a("number").and.be.greaterThan(je.travelOverheadTime)
         });
     });
 
     it("should throw an error if an address is invalid", () =>
     {
-        expect(() => je.getTravelTime("Invalid", "90 Akoranga Dr, Northcote", tomorrow)).to.be.rejectedWith(Error);
+        let expectedError = "Invalid address passed";
+        return expect(je.getTravelTime("Invalid", "90 Akoranga Dr, Northcote", tomorrow)).to.eventually.be.rejectedWith("Invalid source address").then(() =>
+        {
+            return expect(je.getTravelTime("55 Wellesley St, Auckland", "Invalid", tomorrow)).to.eventually.be.rejectedWith("Invalid destination address");
+        });
+    });
+
+    it("should throw an error if an argument is not passed", () =>
+    {
+        let expectedError = "Source, destination and departure date must be specified";
+        return expect(je.getTravelTime(null, "90 Akoranga Dr, Northcote", tomorrow)).to.eventually.be.rejectedWith(expectedError).then(() =>
+        {
+            return expect(je.getTravelTime("55 Wellesley St, Auckland", null, tomorrow)).to.eventually.be.rejectedWith(expectedError);
+        }).then(() =>
+        {
+            return expect(je.getTravelTime("55 Wellesley St, Auckland", "90 Akoranga Dr, Northcote", null)).to.eventually.be.rejectedWith(expectedError);
+        });
+    });
+
+    it("should throw an error if the departure date is in the past", () =>
+    {
+        return expect(je.getTravelTime("55 Wellesley St, Auckland", "90 Akoranga Dr, Northcote", yesterday)).to.eventually.be.rejectedWith("Date cannot be in the past");
     });
 });
